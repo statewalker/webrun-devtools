707cb3f03863f1beaf649c23008eac1d60adf263
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-11-12 12:25:18 +0100

    refactor: make stable connection establishement between extension and pages

--- connectExtensionToPage.js
+++ connectExtensionToPage.js
@@ -3,6 +3,7 @@ import newRegistry from './newRegistry.js';
 const TYPE_CONTENT_CONNECTION = 'content-connection';
 const TYPE_CONNECTION_REQUEST = 'connection-request';
 const TYPE_CONNECTION_RESPONSE = 'connection-response';
+const TYPE_CONNECTION_ERROR = 'connection-error';
 
 /**
  * This wrapper function transforms chrome.runtime.onConnect ports
@@ -55,23 +56,28 @@ export function newConnectionHandler({
  * to establish connection with this extension
  * @returns {function} cleanup function
  */
-export function connectExtensionToPage({
+export async function connectExtensionToPage({
   connectionType = TYPE_CONTENT_CONNECTION,
   tabId,
   secret
 }) {
-  chrome.scripting.executeScript({
+  const callId = `call-${Date.now()}-${Math.random()}`;
+  // Set listeners for messages comming from the page.
+  await chrome.scripting.executeScript({
     target: {
       tabId,
       allFrames: true
     },
     injectImmediately: true,
+    world: 'ISOLATED',
     args: [
       {
         connectionType,
         typeConnectionRequest: TYPE_CONNECTION_REQUEST,
         typeConnectionResponse: TYPE_CONNECTION_RESPONSE,
-        secret
+        typeConnectionError: TYPE_CONNECTION_ERROR,
+        secret,
+        callId
       }
     ],
     // A standalone script without any dependencies.
@@ -80,30 +86,100 @@ export function connectExtensionToPage({
       connectionType,
       typeConnectionRequest,
       typeConnectionResponse,
-      secret
+      typeConnectionError,
+      secret,
+      callId
     }) {
       window.addEventListener('message', async function messageListener(ev) {
-        const { data } = ev;
-        if (data?.type === typeConnectionRequest && data?.secret === secret) {
-          window.removeEventListener('message', messageListener);
-          const port = await chrome.runtime.connect({ name: connectionType });
+        const { data, ports } = ev;
+        if (data?.type === typeConnectionRequest && data?.callId === callId) {
+          const channelPort = ports[0];
+          if (data?.secret === secret) {
+            // window.removeEventListener('message', messageListener);
+            const port = await chrome.runtime.connect({ name: connectionType });
+            port.onMessage.addListener((data) => channelPort.postMessage(data));
+            channelPort.onmessage = ({ data }) => port.postMessage(data);
+            port.onDisconnect.addListener(() => {
+              channelPort.onmessage = null;
+              channelPort.close();
+            });
+            // Reply to the page using the recieved port
+            channelPort.postMessage({ type: typeConnectionResponse, callId });
+          } else {
+            const message =
+              'The page is not authorized to establish connection with this extension.';
+            channelPort.postMessage({
+              type: typeConnectionError,
+              callId,
+              message
+            });
+            channelPort.close();
+            console.warn(message);
+          }
+        }
+      });
+    }
+  });
+
+  // Send a message from the page to establish connection.
+  await chrome.scripting.executeScript({
+    target: {
+      tabId,
+      allFrames: true
+    },
+    injectImmediately: true,
+    world: 'MAIN',
+    args: [
+      {
+        connectionType,
+        typeConnectionRequest: TYPE_CONNECTION_REQUEST,
+        typeConnectionResponse: TYPE_CONNECTION_RESPONSE,
+        typeConnectionError: TYPE_CONNECTION_ERROR,
+        callId
+      }
+    ],
+    // A standalone script without any dependencies.
+    // So we can not use imported / global statements here.
+    func: async function injectedFunction({
+      typeConnectionRequest,
+      typeConnectionError,
+      callId
+    }) {
+      if (!window.___debuggerPromise) {
+        let resolve, reject;
+        window.___debuggerPromise = new Promise(
+          (y, n) => ((resolve = y), (reject = n))
+        );
+        window.___debuggerPromise.resolve = resolve;
+        window.___debuggerPromise.reject = reject;
+      }
+      window.___debuggerPromise.resolve(async ({ secret } = {}) => {
+        return new Promise((resolve, reject) => {
           const channel = new MessageChannel();
-          port.onMessage.addListener((data) => channel.port1.postMessage(data));
-          channel.port1.onmessage = ({ data }) => port.postMessage(data);
-          port.onDisconnect.addListener(() => {
+          channel.port1.onmessage = function recieveResponse({ data }) {
+            if (data.callId !== callId) return; // It should never happen
+            // This is the response from the ISOLATED world. It happens once.
             channel.port1.onmessage = null;
-            channel.port1.close();
-            channel.port2.close();
-          });
+            if (data.type === typeConnectionError) {
+              reject(new Error(data.message));
+            } else {
+              resolve(channel.port1);
+            }
+          };
+          // Send the pair port to the ISOLATED world.
+          // We will wait for the reply before returning.
+          // It guaranties us that connections to the background
+          // is already established.
           window.postMessage(
             {
-              ...data,
-              type: typeConnectionResponse
+              type: typeConnectionRequest,
+              secret,
+              callId
             },
             '*',
             [channel.port2]
           );
-        }
+        });
       });
     }
   });
--- connectPageToExtension.js
+++ connectPageToExtension.js
@@ -1,61 +1,18 @@
 // import * as Comlink from 'https://unpkg.com/comlink/dist/esm/comlink.mjs';
 import * as Comlink from './comlink.js';
-import newRegistry from './newRegistry.js';
-const TYPE_CONNECTION_REQUEST = 'connection-request';
-const TYPE_CONNECTION_RESPONSE = 'connection-response';
 
-export async function connectPageToExtension({
-  secret,
-  onEvent = () => {},
-  timeout = 1000 * 30, // 30 seconds,
-  preloadTimeout = 30
-}) {
-  // Await document.readyState === 'complete'
-  await new Promise((resolve) => {
-    if (document.readyState === 'loading') {
-      // Loading hasn't finished yet
-      document.addEventListener('DOMContentLoaded', resolve);
-    } else {
-      // `DOMContentLoaded` has already fired
-      resolve();
-    }
-  });
-  // Await a little bit before sending the message.
-  await new Promise((resolve) => setTimeout(resolve, preloadTimeout));
-
-  // // Request next animation frame before sending the message.
-  // await new Promise((resolve) => requestAnimationFrame(resolve));
-  const [register, cleanup] = newRegistry();
-  const callId = `call-${Date.now()}-${Math.random()}`;
-  const promise = new Promise((resolve, reject) => {
-    const timerId = setTimeout(
-      () => reject({ message: 'Connection timeout' }),
-      timeout
+export async function connectPageToExtension({ secret, onEvent = () => {} }) {
+  if (!window.___debuggerPromise) {
+    let resolve, reject;
+    window.___debuggerPromise = new Promise(
+      (y, n) => ((resolve = y), (reject = n))
     );
-    register(() => clearTimeout(timerId));
-
-    function messageListener(ev) {
-      const { data, ports } = ev;
-      if (data.type === TYPE_CONNECTION_RESPONSE && data.callId === callId) {
-        const [port] = ports;
-        Comlink.expose(onEvent, port);
-        const api = Comlink.wrap(port);
-        resolve(api);
-      }
-    }
-    window.addEventListener('message', messageListener);
-    register(() => window.removeEventListener('message', messageListener));
-
-    window.postMessage(
-      {
-        type: TYPE_CONNECTION_REQUEST,
-        secret,
-        callId
-      },
-      '*'
-    );
-  });
-  promise.finally(cleanup);
-
-  return promise;
+    window.___debuggerPromise.resolve = resolve;
+    window.___debuggerPromise.reject = reject;
+  }
+  const getPort = await window.___debuggerPromise;
+  const port = await getPort({ secret });
+  Comlink.expose(onEvent, port);
+  const api = Comlink.wrap(port);
+  return api;
 }
